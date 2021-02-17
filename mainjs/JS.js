ef555a840453bbe96c6b231b9a5b7ba2d87afcd2
Ela <ela.git89@gmail.com>
2021-02-17 18:32:46 +0100

    bledne kodowanie

--- mainjs/JS.js
+++ mainjs/JS.js
@@ -36,71 +36,174 @@ console.log(addSeasonImage(date));
 
 
 const cityName = document.querySelector('.cityName');
-const currentDate = document.querySelector('.currentDate');
-const currentTemperature = document.querySelector('.currentTemperature');
-const mainIcon = document.querySelector('.mainIcon');
+const zeroDay = document.querySelector('.zeroDay');
+const tempZero = document.querySelector('.tempZero');
+const iconZero = document.querySelector('.iconZero');
 const currentPressure = document.querySelector('.currentPressure');
 const currentWindSpeed = document.querySelector('.currentWindSpeed');
 const precipitationProbability = document.querySelector('.precipitationProbability');
+const firstDay = document.querySelector('.firstDay');
+const secondDay = document.querySelector('.secondDay');
+const thirdDay = document.querySelector('.thirdDay');
+const fourthDay = document.querySelector('.fourthDay');
+const fifthDay = document.querySelector('.fifthDay');
+const sixthDay = document.querySelector('.sixthDay');
+const iconOne = document.querySelector('.iconOne');
+const iconTwo = document.querySelector('.iconTwo');
+const iconThree = document.querySelector('.iconThree');
+const iconFour = document.querySelector('.iconFour');
+const iconFife = document.querySelector('.iconFife');
+const iconSix = document.querySelector('.iconSix');
+const tempOne = document.querySelector('.tempOne');
+const tempTwo = document.querySelector('.tempTwo');
+const tempThree = document.querySelector('.tempThree');
+const tempFour = document.querySelector('.tempFour');
+const tempFife = document.querySelector('.tempFife');
+const tempSix = document.querySelector('.tempSix');
 
-function setCurrentDate(weatherData){
-  const today = weatherData.data[0].datetime;
-  const year = Number(today.slice(0,4));
-  const month = Number(today.slice(5,7))-1;
-  const day = Number(today.slice(8));
-  const actualDate = new Date(year,month,day);
-  const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-  currentDate.textContent = actualDate.toLocaleDateString('pl-PL', options);
-}
 
-function setTemperature(weatherData){
-  const temperature = weatherData.data[0].temp;
-  currentTemperature.textContent = `${temperature} \u2103`;
+class WeatherForecast {
+  constructor(weatherForDay, numberOfDay,numberOfIcon, numberOfTemp){
+  this.weatherForDay = weatherForDay;
+  this.numberOfDay = numberOfDay;
+  this.numberOfIcon = numberOfIcon;
+  this.numberOfTemp = numberOfTemp;
+  
+  }
+  changeFormatOfDate(){
+    const date = this.weatherForDay.datetime;
+    const year = Number(date.slice(0,4));
+    const month = Number(date.slice(5,7))-1;
+    const day = Number(date.slice(8));
+    const actualDate = new Date(year,month,day);
+    return actualDate;
+  }
+  setDateInDom(){
+    const actualDate = this.changeFormatOfDate();
+    const options = {weekday: 'long'};
+    this.numberOfDay.textContent = actualDate.toLocaleDateString('pl-PL', options);
+  }
+
+
+  setIcon(){
+    const icon = this.weatherForDay.weather.icon;
+    this.numberOfIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+  }
+
+  setTemperature(){
+      const temperature = this.weatherForDay.temp;
+      this.numberOfTemp.textContent = `${temperature} \u2103`;
+      
+    }
+    
   
 }
 
-function setIcon(weatherData){
-  icon = weatherData.data[0].weather.icon;
-  mainIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+
+class CurrentWeatherForecast extends WeatherForecast{
+  constructor(weatherForDay, numberOfDay,numberOfIcon, numberOfTemp, pressure, windSpeed, precipitation){
+    super(weatherForDay, numberOfDay,numberOfIcon, numberOfTemp);
+    this.pressure = pressure;
+    this.windSpeed = windSpeed;
+    this.precipitation = precipitation;
+  }
+
+ 
+
+  setDateInDom(){
+    const actualDate = this.changeFormatOfDate();
+    const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    this.numberOfDay.textContent = actualDate.toLocaleDateString('pl-PL', options);
+  }
+
+
+ setCurrentPressure(){
+    const press = (this.weatherForDay.pres).toFixed(2);
+    console.log(typeof press);
+    this.pressure.textContent = `ciśnienie: ${press} hPa`;
+  }
   
-}
-
-function setCurrentPressure(weatherData){
-  const pressure = (weatherData.data[0].pres).toFixed(2);
-  currentPressure.textContent = `ciśnienie: ${pressure} hPa`;
-
-}
-
-function setWindSpeed(weatherData){
-  const windSpeed = Number(weatherData.data[0].wind_spd);
-  const windSpeedKmPerHour = ((windSpeed * 100000) * (3.6 * 10)/1000000).toFixed(2);
-  currentWindSpeed.textContent = `prędkość wiatru: ${windSpeedKmPerHour} km/h`;
-
-
-}
-
-function setPrecipitationProbability(weatherData){
-
-  const precipitationChance = weatherData.data[0].pop;
-  precipitationProbability.textContent = `prawdopodobieństwo opadów: ${precipitationChance} \u0025`;
+  setWindSpeed(){
+    const windSpeed = Number(this.weatherForDay.wind_spd);
+    const windSpeedKmPerHour = ((windSpeed * 100000) * (3.6 * 10)/1000000).toFixed(2);
+    this.windSpeed.textContent = `prędkość wiatru: ${windSpeedKmPerHour} km/h`;
+  
+  
+  }
+  
+  setPrecipitationProbability(){
+  
+    const precipitationChance = this.weatherForDay.pop;
+    this.precipitation.textContent = `prawdopodobieństwo opadów: ${precipitationChance} \u0025`;
+  
+  }
+  
 
 }
 
 function showWeatherForTheCity(weatherData){
-  cityName.textContent = weatherData.city_name;
-  
-  setCurrentDate(weatherData);
-  setTemperature(weatherData);
-  setIcon(weatherData);
-  setCurrentPressure(weatherData);
-  setWindSpeed(weatherData);
-  setPrecipitationProbability(weatherData);
-  
-  
   console.log(weatherData);
+  cityName.textContent = weatherData.city_name;
 
+  const weatherForZeroDay = weatherData.data[0];
+  const weatherForFirstDay = weatherData.data[1];
+  const weatherForSecondDay = weatherData.data[2];
+  const weatherForThirdDay = weatherData.data[3];
+  const weatherForFourthDay = weatherData.data[4];
+  const weatherForFifthDay = weatherData.data[5];
+  const weatherForSixthDay = weatherData.data[6];
+
+  const currentWeatherForecast1 = new CurrentWeatherForecast(weatherForZeroDay, zeroDay, iconZero,tempZero, currentPressure, currentWindSpeed, precipitationProbability );
+  const weatherForecast1 = new WeatherForecast(weatherForFirstDay, firstDay, iconOne,tempOne);
+  const weatherForecast2 = new WeatherForecast(weatherForSecondDay, secondDay, iconTwo,tempTwo);
+  const weatherForecast3 = new WeatherForecast(weatherForThirdDay, thirdDay, iconThree,tempThree);
+  const weatherForecast4 = new WeatherForecast(weatherForFourthDay, fourthDay, iconFour,tempFour);
+  const weatherForecast5 = new WeatherForecast(weatherForFifthDay, fifthDay, iconFife,tempFife);
+  const weatherForecast6 = new WeatherForecast(weatherForSixthDay, sixthDay, iconSix,tempSix);
+
+
+  console.log (currentWeatherForecast1.changeFormatOfDate());
+  console.log (currentWeatherForecast1.setDateInDom());
+  console.log(currentWeatherForecast1.setIcon());
+  console.log(currentWeatherForecast1.setTemperature());
+  console.log(currentWeatherForecast1.setCurrentPressure());
+  console.log(currentWeatherForecast1.setWindSpeed());
+  console.log(currentWeatherForecast1.setPrecipitationProbability());
+  
+  console.log (weatherForecast1.changeFormatOfDate());
+  console.log (weatherForecast1.setDateInDom());
+  console.log(weatherForecast1.setIcon());
+  console.log(weatherForecast1.setTemperature());
+
+  console.log (weatherForecast2.changeFormatOfDate());
+  console.log (weatherForecast2.setDateInDom());
+  console.log(weatherForecast2.setIcon());
+  console.log(weatherForecast2.setTemperature());
+
+  console.log (weatherForecast3.changeFormatOfDate());
+  console.log (weatherForecast3.setDateInDom());
+  console.log(weatherForecast3.setIcon());
+  console.log(weatherForecast3.setTemperature());
+
+  console.log (weatherForecast4.changeFormatOfDate());
+  console.log (weatherForecast4.setDateInDom());
+  console.log(weatherForecast4.setIcon());
+  console.log(weatherForecast4.setTemperature());
+
+  console.log (weatherForecast5.changeFormatOfDate());
+  console.log (weatherForecast5.setDateInDom());
+  console.log(weatherForecast5.setIcon());
+  console.log(weatherForecast5.setTemperature());
+
+  console.log (weatherForecast6.changeFormatOfDate());
+  console.log (weatherForecast6.setDateInDom());
+  console.log(weatherForecast6.setIcon());
+  console.log(weatherForecast6.setTemperature());
+
+  
+
+  
 }
-
 
 function sendStartRequest(){
 
